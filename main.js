7ebb5c13ab586d65a6d5e55ace1003973c7f0989
p0lygun <solankivibhakar82@gmail.com>
2022-07-20 02:15:44 +0530

    feat: add handling for localhost

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ window.addEventListener('load', function () {
   ];
 
   ids.forEach(async function(elem, index){
-      await fetch(`http://vmi656705.contaboserver.net:5000/user/?id=${elem}&guild_id=998810232224493568`).then(
+      const loc = new URL(window.document.location)
+      let hostname = loc.hostname,
+      guild_id = "998810232224493568";
+
+      if (loc.hostname === "0.0.0.0"){
+          guild_id = "829265067073339403";
+      }
+      await fetch(`http://${hostname}:5000/user/?id=${elem}&guild_id=${guild_id}`).then(
           resp => resp.json().then(json => {
               if(json['status']) {
                   const item = document.getElementById(elem);
